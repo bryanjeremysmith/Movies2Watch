14b30bcaabb26a99552f60bbdfb581fec8f9928f
BJ Smith <bryanjeremysmith@gmail.com>
2022-11-30 12:35:50 -0800

    also querying OMDB, and using the results to get the year of the movie, and appending that year and " trailer" to the query to youtube (to help get the correct result from YouTube)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
-var baseURL = "https://www.googleapis.com/youtube/v3";
-var apiKey = "AIzaSyAS3ZMf20tOKozGt4fbv5HHPCGhFEZFuco";
+var baseYouTubeURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAS3ZMf20tOKozGt4fbv5HHPCGhFEZFuco";
+var baseIMDBURL = "http://www.omdbapi.com/?apikey=9e1ba2cf&";
 
 // Search for a specified string.
 function search() {
     var q = $('#query').val();
 
-    var request = baseURL + "/" + "search?q=" + q + "&part=snippet&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&maxResults=1&key=" + apiKey;
+    searchIMDB(q);
+}
 
-    fetch(request)
+function searchIMDB(q){
+    var requestIMDB = baseIMDBURL + "&t=" + q + "&plot=short";
+    fetch(requestIMDB)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        $("#movie-title").text(data.Title);
+        $("#movie-plot").text(data.Plot);
+        searchYouTube(q + " " + data.Year);
+    })
+}
+
+function searchYouTube(q){
+    var requestYouTube = baseYouTubeURL + 
+        "&q=" + q + " trailer" + 
+        "&part=snippet" + 
+        "&order=rating" + 
+        "&type=video" + 
+        "&videoDefinition=high" + 
+        "&videoEmbeddable=true" +
+        "&maxResults=1";
+
+    fetch(requestYouTube)
     .then(function (response) {
         return response.json();
     })
     .then(function (data){
-        $('#embedded-video').html('<iframe src="https://www.youtube.com/embed/' + data.items[0].id.videoId + '?autoplay=1&mute=1"></iframe>');
+        $('#movie-embedded-video').html('<iframe src="https://www.youtube.com/embed/' + data.items[0].id.videoId + '?autoplay=1&mute=1"></iframe>');
     });
 }
