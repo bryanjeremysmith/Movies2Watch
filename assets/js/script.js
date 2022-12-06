871ac31806537432b1172a2f238daac84431ebe9
Sam Jensen <samual.jensen@gmail.com>
2022-12-05 20:39:40 -0800

    added to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ var baseYouTubeURL = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAS3
 var baseIMDBURL = "http://www.omdbapi.com/?apikey=9e1ba2cf&";
 let movieTitleText = JSON.parse(window.localStorage.getItem("movieTitleText")) || [];
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#cardHolder").hide();
-    $("#smallSearch").hide();           
+    $("#smallSearch").hide();
 });
 
 function search() {
@@ -19,31 +19,43 @@ function appendToMoviesList() {
     var newMovie = document.createElement("li");
     newMovie.textContent = $("#movie-title").text();
     movieList.appendChild(newMovie);
-    
+
     saveMovieTitle();
-    
-    newMovie.addEventListener("click", function(){
+
+    newMovie.addEventListener("click", function () {
         search();
 
         var q = this.textContent;
 
         searchIMDB(q);
-        
+
     });
     // remove, add text and class to the watch to list button 
     $(".cardBtnHolder > .button").removeClass("addBtnText");
     $(".cardBtnHolder > .button").text("✓ Added to the List");
     $(".cardBtnHolder > .button").addClass("addGreenBtnText");
-    
+
 };
 
-function saveMovieTitle () {
+function saveMovieTitle() {
     let movieVal = $("#movie-title").text();
     movieTitleText.unshift(movieVal);
     window.localStorage.setItem("movieTitleText", JSON.stringify(movieTitleText));
-    $("#movie-title").text(movieTitleText);
+    // $("#movie-title").text(movieTitleText);
     console.log(movieTitleText);
 };
+
+function getMovieList() {
+    movieTitleText = window.localStorage.getItem("movieTitleText");
+    var movieList = document.querySelector("#moviesList");
+    for (let i = 0; i < movieTitleText.length; i++) {
+        var newMovie = document.createElement("li");
+        newMovie.textContent = movieTitleText[i];
+        movieList.appendChild(newMovie);
+    }
+};
+
+
 
 function searchAPIs() {
     search();
@@ -61,58 +73,56 @@ function searchAPIsSmall() {
     searchIMDB(q);
 }
 
-function searchIMDB(q){
+function searchIMDB(q) {
     var requestIMDB = baseIMDBURL + "&t=" + q + "&plot=short";
     fetch(requestIMDB)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        if(data.Response === "False")
-        {
-            $("#movie-title").text(data.Error);
-            $("#movie-plot").text("");
-            $('#movie-embedded-video').html("");
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data.Response === "False") {
+                $("#movie-title").text(data.Error);
+                $("#movie-plot").text("");
+                $('#movie-embedded-video').html("");
 
-            $("#watch-trailer").addClass("hide");
-            $("#add-movie").addClass("hide");
-            $("#cardHolder").show();
-        }
-        else
-        {
-            $("#movie-title").text(data.Title);
-            $("#movie-plot").text(data.Plot);
-            $("#watch-trailer").addClass("visible");
-            $("#add-movie").addClass("visible");
-            searchYouTube(q + " " + data.Year);
-        }
-    })
-    .catch(function (error){
-        console.log(error);
-    });
+                $("#watch-trailer").addClass("hide");
+                $("#add-movie").addClass("hide");
+                $("#cardHolder").show();
+            }
+            else {
+                $("#movie-title").text(data.Title);
+                $("#movie-plot").text(data.Plot);
+                $("#watch-trailer").addClass("visible");
+                $("#add-movie").addClass("visible");
+                searchYouTube(q + " " + data.Year);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
-function searchYouTube(q){
-    var requestYouTube = baseYouTubeURL + 
-        "&q=" + q + " trailer" + 
-        "&part=snippet" + 
-        "&order=rating" + 
-        "&type=video" + 
-        "&videoDefinition=high" + 
+function searchYouTube(q) {
+    var requestYouTube = baseYouTubeURL +
+        "&q=" + q + " trailer" +
+        "&part=snippet" +
+        "&order=rating" +
+        "&type=video" +
+        "&videoDefinition=high" +
         "&videoEmbeddable=true" +
         "&maxResults=1";
 
     fetch(requestYouTube)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data){
-        $('#movie-embedded-video').html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + data.items[0].id.videoId + '?autoplay=1&mute=1"></iframe>');
-        $("#cardHolder").show();
-    })
-    .catch(function (error){
-        console.log(error);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            $('#movie-embedded-video').html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + data.items[0].id.videoId + '?autoplay=1&mute=1"></iframe>');
+            $("#cardHolder").show();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 // enter key 
